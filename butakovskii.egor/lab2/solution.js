9707a986efeb520e806de5d029a9ce8ee9c48783
egor butakovskii <mrtntkatz@gmail.com>
2026-09-14 21:00:16 +0300

    prettier and eslint

--- butakovskii.egor/lab2/solution.js
+++ butakovskii.egor/lab2/solution.js
@@ -1,17 +1,21 @@
 // Экспортируйте отсюда функцию с именем из контракта вашего варианта.
 function zipArrays(...arrays) {
-    if (arrays.length === 0) return [];
+  if (arrays.length === 0) {
+    return [];
+  }
 
-    if (arrays.every(arr => arr.length === 0)) return [];
+  if (arrays.every((arr) => arr.length === 0)) {
+    return [];
+  }
 
-    const maxLength = Math.max(...arrays.map(arr => arr.length));
-    const result = [];
+  const maxLength = Math.max(...arrays.map((arr) => arr.length));
+  const result = [];
 
-    for (let i = 0; i < maxLength; i++) {
-        result.push(arrays.map(arr => arr[i]));
-    }
+  for (let i = 0; i < maxLength; i++) {
+    result.push(arrays.map((arr) => arr[i]));
+  }
 
-    return result;
+  return result;
 }
 
 export {zipArrays};
