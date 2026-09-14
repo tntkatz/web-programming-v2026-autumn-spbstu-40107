2ff61c0389bda8b28b28411190539a4518cfa921
egor butakovskii <mrtntkatz@gmail.com>
2026-09-14 20:56:12 +0300

    first commit

---
+++ butakovskii.egor/lab2/solution.js
@@ -0,0 +1,17 @@
+// Экспортируйте отсюда функцию с именем из контракта вашего варианта.
+function zipArrays(...arrays) {
+    if (arrays.length === 0) return [];
+
+    if (arrays.every(arr => arr.length === 0)) return [];
+
+    const maxLength = Math.max(...arrays.map(arr => arr.length));
+    const result = [];
+
+    for (let i = 0; i < maxLength; i++) {
+        result.push(arrays.map(arr => arr[i]));
+    }
+
+    return result;
+}
+
+export {zipArrays};
